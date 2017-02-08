343063342eed63b4209f056c1d7a867d28076db9
(cdep)illabout <cdep.illabout@gmail.com>
2017-02-08 15:56:28 +0900

    Add documentation and build constraints.

--- src/Text/XML/QQ.hs
+++ src/Text/XML/QQ.hs
@@ -11,7 +11,7 @@ Maintainer  :  Dennis Gosnell (cdep.illabout@gmail.com)
 Stability   :  experimental
 Portability :  unknown
 
-This module provides a quasi-quoter for XML 'Document's.
+This module provides a quasi-quoter for XML 'Document's.  See the 'xml' function for some examples.
 -}
 
 module Text.XML.QQ
@@ -34,14 +34,57 @@ import Text.XML (Document(..), parseText)
 import Text.XMLHTML.Internal
        (createExpQuasiQuoter, handleParseDocErr)
 
+-- $setup
+-- >>> :set -XQuasiQuotes
+-- >>> :set -XTemplateHaskell
+
+
 -- | This 'QuasiQuoter' produces XML 'Document's.
 --
--- The type of an expression that this 'QuasiQuoter' produces is @'Either' 'SomeException' 'Document'@.  It produces a @'Left' 'SomeException'@ when the document cannot be parsed.
+-- This 'QuasiQuoter' produces expressions of type
+-- @'Either' 'SomeException' 'Document'@.  It produces a
+-- @'Left' 'SomeException'@ when the input string cannot be parsed into an XML
+-- 'Document'.
+--
+-- Here's a simple example of using it:
+--
+-- >>> [xml|<html></html>|]
+-- Right (Document {documentPrologue = Prologue {prologueBefore = [], prologueDoctype = Nothing, prologueAfter = []}, documentRoot = Element {elementName = Name {nameLocalName = "html", nameNamespace = Nothing, namePrefix = Nothing}, elementAttributes = fromList [], elementNodes = []}, documentEpilogue = []})
+--
+-- Internally, this function is using the
+-- <https://hackage.haskell.org/package/heterocephalus heterocephalus> package.
+-- This means you can use variable interpolation, as well as @forall@, @if@,
+-- and @case@ control statements.  Checkout the
+-- <https://github.com/arowM/heterocephalus#syntax heterocephalus README> for
+-- more info.
+--
+-- >>> let a = "hello world"
+-- >>> [xml|<html>#{a}</html>|]
+-- Right ...
+--
+-- Here's an example of a template that will produce a 'Left' value:
+--
+-- >>> [xml|<html </html>|]
+-- Left ...
+--
+-- Here's an example of a template that can be parsed as an HTML 'Document', but
+-- not as an XML 'Document':
+--
+-- >>> [xml|<html><br></html>|]
+-- Left ...
 xml :: QuasiQuoter
 xml =
   createExpQuasiQuoter $ \string ->
     appE [|parseText def . renderMarkup|] $ compileFromString textSetting string
 
+-- | This function is just like 'xml', but produces expressions of type
+-- 'Document'.
+--
+-- If your input string cannot be parsed into a valid 'Document', an error will
+-- be thrown at runtime with 'error'.
+--
+-- This function is nice to use in GHCi or tests, but should not be used in
+-- production code.
 xmlUnsafe :: QuasiQuoter
 xmlUnsafe =
   createExpQuasiQuoter $ \string ->
@@ -50,6 +93,11 @@ xmlUnsafe =
         parseText def . renderMarkup|]
       (compileFromString textSetting string)
 
+-- | This function is similar to 'xml', but doesn't allow variable interpolation
+-- or control statements.  It produces expressions of type 'Document'.
+--
+-- An error will be thrown at compile-time if the input string cannot be parsed
+-- into a 'Document'.
 xmlRaw :: QuasiQuoter
 xmlRaw =
   createExpQuasiQuoter $ \string ->
